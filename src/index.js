3bf8cc8a5d9f2fd5e0434f45f10419e1bfb08e7b
william duan <38791932+duanwilliam@users.noreply.github.com>
2021-06-20 21:07:34 -0700

    refactor: update tab import checking to match docusaurus npm2yarn

--- src/index.js
+++ src/index.js
@@ -106,18 +106,10 @@ const transformNode = (node, { sync, tabLabels, fileBasePath }) => {
   return res;
 }
 
-const importTabsRegex = /import\s+Tabs\s+from\s+(["'`])@theme\/Tabs\1/m;
-const importTabItemRegex = /import\s+TabItem\s+from\s+(["'`])@theme\/TabItem\1/m;
-const isImportTabsNode = (node) => node.type === 'import' && importTabsRegex.test(node.value);
-const isImportTabItemNode = (node) => node.type === 'import' && importTabItemRegex.test(node.value);
-const importTabsNode = {
+const importNode = {
   type: 'import',
-  value: "import Tabs from '@theme/Tabs';",
+  value: "import Tabs from '@theme/Tabs';\nimport TabItem from '@theme/TabItem';",
 };
-const importTabItemNode = {
-  type: 'import',
-  value: "import TabItem from '@theme/TabItem';",
-}
 
 const isCodetabsNode = (node) => node.type === 'code' && node.meta === 'codetabs';
 
@@ -136,15 +128,11 @@ const attacher = (options = {}) => {
   };
 
   let transformed = false;
-  let importTabsNodeExists = false;
-  let importTabItemNodeExists = false;
+  let alreadyImported = false;
 
   const transformer = (node) => {
-    if (isImportTabsNode(node)) {
-      importTabsNodeExists = true;
-    }
-    if (isImportTabItemNode(node)) {
-      importTabItemNodeExists = true;
+    if (node.type === 'import' && node.value.includes('@theme/Tabs')) {
+      alreadyImported = true;
     }
 
     if (isCodetabsNode(node)) {
@@ -163,13 +151,8 @@ const attacher = (options = {}) => {
         }
       }
     }
-    if (node.type === 'root' && transformed) {
-      if(!importTabItemNodeExists) {
-        node.children.unshift(importTabItemNode);
-      }
-      if(!importTabsNodeExists) {
-        node.children.unshift(importTabsNode);
-      }
+    if (node.type === 'root' && transformed && !alreadyImported) {
+      node.children.unshift(importNode);
     }
     return undefined;
   };
